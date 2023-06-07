ed41c5384b65d8fe37bf44eeb94e6d48a479bc3f
Dhruv <sharmadhruv272@gmail.com>
2023-06-07 21:12:57 +0530

    Implemented the database for storing a successful order

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,9 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'));
+// app.use('/api', require('./routes/auth'));
+app.use('/api/order', require('./routes/order'));
+
 
 app.get('/', (req,res) => 
     res.send('Hello Customer!')
