d56a84444a0c4dee6e8153cad72d3937e359fb4f
AtulRatnawat <atulratnawat100@gmail.com>
2023-06-16 22:22:01 +0530

    Created a Basket Dropdown with profile option and logout option and displayed the results on the button on admin dashboard. Also Created an empty User profile.

--- frontend/src/components-admin/Navbar/Navbar.js
+++ frontend/src/components-admin/Navbar/Navbar.js
@@ -1,59 +1,89 @@
-import React from 'react'
-import basket from '../../images/basket_navbar.gif'
-import logo from '../../images/Logo.png'
-import './Navbar.css'
-import { NavLink, useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import basket from '../../images/basket_navbar.gif';
+import logo from '../../images/Logo.png';
+import './Navbar.css';
+import { NavLink, useNavigate } from 'react-router-dom';
 
-function Navbar() 
-{
+function Navbar() {
   const navigate = useNavigate();
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handleLogout = () => {
     localStorage.removeItem('token');
-    // localStorage.removeItem('currentUser');
     navigate('/');
-  }
+  };
 
-  const user = JSON.parse(localStorage.getItem("currentUser"));
-  const userName = (user.userEmail).split("@")[0];
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+  };
+
+  const user = JSON.parse(localStorage.getItem('currentUser'));
+  const userName = user?.userEmail.split('@')[0];
+
   return (
     <>
-        <nav className="navbar navbar-expand-sm navbar-light nav_page" style={{fontSize: '25px'}} >
-          <div className="container-fluid ">
-          <NavLink className='navbar-brand' to='/home'><img className='logo' src={logo} alt="" /></NavLink>
+      <nav className="navbar navbar-expand-sm navbar-light nav_page" style={{ fontSize: '25px' }}>
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/home">
+            <img className="logo" src={logo} alt="" />
+          </NavLink>
 
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div className="collapse navbar-collapse justify-content-end"  id="navbarSupportedContent">
+          <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
             <ul className="navbar-nav">
-                <li className="nav-item  navbar_items">
-                <NavLink className="nav-link " aria-current="page" to="/home">Welcome {userName}</NavLink>
-                </li>
-                {/* <li className="nav-item  navbar_items">
-                  <NavLink className="nav-link " aria-current="page" to="/home">Home</NavLink>
-                </li> */}
-                <li className="nav-item  navbar_items">
-                  <NavLink className="nav-link" to="/admin-dashboard">Dashboard</NavLink>
-                </li>			
-                <li className="nav-item  navbar_items" id='next_line'>
-                  <NavLink className="nav-link " to="/admin-orders">Orders</NavLink>
-                </li>			
-                <li className="nav-item  navbar_items">
-                  <NavLink className="nav-link" to="/customer-support">Support</NavLink>
-                </li>			
-                <li className="nav-item ">
-                  <NavLink to="/" type="button" onClick={handleLogout}>
-                    <img style={{height:'45px'}} src={basket} alt="" />
-                  </NavLink>
-                </li>			
-            </ul>		  
+              <li className="nav-item  navbar_items">
+                <NavLink className="nav-link" to="/admin-dashboard">
+                  Dashboard
+                </NavLink>
+              </li>
+              <li className="nav-item  navbar_items" id="next_line">
+                <NavLink className="nav-link " to="/admin-orders">
+                  Orders
+                </NavLink>
+              </li>
+              <li className="nav-item  navbar_items">
+                <NavLink className="nav-link" to="/customer-support">
+                  Support
+                </NavLink>
+              </li>
+              <li className="nav-item dropdown" onClick={toggleDropdown}>
+                <img style={{ height: '45px', cursor: 'pointer' }} src={basket} alt="" />
+                {isDropdownOpen && (
+                  <ul className="dropdown-menu show" aria-labelledby="basketDropdown" onClick={closeDropdown}>
+                    <li>
+                      <NavLink className="dropdown-item" to="/user-profile">
+                        Profile
+                      </NavLink>
+                    </li>
+                    <li>
+                      <button className="dropdown-item" onClick={handleLogout}>
+                        Logout
+                      </button>
+                    </li>
+                  </ul>
+                )}
+              </li>
+            </ul>
           </div>
         </div>
-	</nav>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
