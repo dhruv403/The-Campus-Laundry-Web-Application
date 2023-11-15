c27990c45853f23a9541b6d26f23eebb95694c99
Dhruv <sharmadhruv272@gmail.com>
2023-11-15 18:52:37 +0530

    Create user model with subscription field

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -59,15 +59,12 @@ const OrderSchema = new Schema({
   },
   razorpay_order_id:{
     type:String,
-    required:true,
 },
 razorpay_payment_id:{
     type:String,
-    required:true,
 },
 razorpay_signature:{
     type:String,
-    required:true,
 },
 });
 
