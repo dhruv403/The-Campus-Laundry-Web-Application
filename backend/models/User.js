8d99a206aca1726c8265f58fb954e01a5c90cb0e
Dhruv <sharmadhruv272@gmail.com>
2023-11-15 18:52:00 +0530

    Initialize subscription feature

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose');
 
-const {Schema}= mongoose;
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-    name:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  subscription: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const User = mongoose.model('user', UserSchema);
-module.exports = User
+module.exports = User;
